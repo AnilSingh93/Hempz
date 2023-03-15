20ca961f4dac0de5c654671fc5b1d35b77164e76
Anil Singh <AnilSingh93>
2023-03-15 17:03:03 +0530

    validation

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,11 @@
 var $form = $(".sign_up .signup_column form")
-$(".sign_left form button").on("click", function () {
+$(".sign_left form button").on("click", function (e) {
     var toReturn = true;
    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-     $("select", $form).each(function(){
+     $("select, input[type='text']", $form).each(function(){
         if($(this).val() == ""){
             $(this).next().addClass("err_msgs");
+            e.preventDefault();
             toReturn = false
         }
         else{
@@ -14,7 +15,7 @@ $(".sign_left form button").on("click", function () {
        
     })
 
-     $("input[type='email']", $form).each(function(){
+    $("input[type='email']", $form).each(function(){
         if($(this).val().match(validRegex)){
             $(this).next().removeClass("err_msgs");
             return toReturn = true;
@@ -22,20 +23,22 @@ $(".sign_left form button").on("click", function () {
         else{
             $(this).next().addClass("err_msgs");
              toReturn = false
+             e.preventDefault();
         }
     })
 
-  $("input[type='checkbox']", $form).each(function(){
-         if($(this).is(":checked")){
-            $(this).parent(".shogun-form-checkbox-input-wrapper").next().removeClass("err_msgs");
-            return toReturn = true;
-        }
-        else{
-            $(this).parent(".shogun-form-checkbox-input-wrapper").next().addClass("err_msgs")
-            toReturn = false
-        }
-    })
-  
+    $("input[type='checkbox']", $form).each(function(){
+        if($(this).is(":checked")){
+           $(this).parent(".checkbox").next("p.error").removeClass("err_msgs");
+           return toReturn = true;
+       }
+       else{
+        $(this).parent(".checkbox").next("p.error").addClass("err_msgs")
+           toReturn = false
+           e.preventDefault();
+       }
+   })
+
   
  return toReturn = true;
   
